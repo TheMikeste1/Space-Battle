332725894a78c2ccb0a34c8a8ba2404a668c24c9
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2019-07-11 11:39:39 -0600

    Added Bullet ID
    Added and ID to Bullet to more easily identify it.

--- Bullet.h
+++ Bullet.h
@@ -16,24 +16,33 @@
 class Bullet : public Object
 {
 private:
+	unsigned int id;
 	int decay; //When decay == 40, Bullet dies.
 
 public:
 	//Constructors
-	Bullet() : decay(0) {}
-	Bullet(int decay, Point point, Velocity velocity):
-		decay(decay) { setPoint(point); setVelocity(velocity); }
+	Bullet() : decay(0) { id = random(1, 9999999); }
+	Bullet(int id, int decay, Point point, Velocity velocity):
+		id(id), decay(decay) 
+	{ setPoint(point); setVelocity(velocity); }
 	//Deconstructors
 	~Bullet() {}
 
 	//Getters
 	int getDecay() const { return decay; }
+	int getId() const { return id; }
+
 	//Setters
 
 	//Others
 	void fire(Point point, float angle, Velocity velocity);
 	void draw() override;
 };
+
+inline bool operator==(unsigned int lhs, const Bullet& rhs)
+{
+	return lhs = rhs.getId();
+}
 
 inline bool operator==(const Bullet& lhs, const Bullet& rhs)
 {
